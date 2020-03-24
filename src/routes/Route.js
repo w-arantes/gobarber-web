391ccc8841830435ec20f71b571421e08818e02f
w-arantes <w.aranteslfc@gmail.com>
2020-03-24 12:44:02 -0300

    chore: auth params

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -5,12 +5,14 @@ import { Route, Redirect } from 'react-router-dom';
 import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
 
+import store from '~/store';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = false;
+  const { signed } = store.getState().auth;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
@@ -25,7 +27,7 @@ export default function RouteWrapper({
   return (
     <Route
       {...rest}
-      render={(props) => (
+      render={props => (
         <Layout>
           <Component {...props} />
         </Layout>
