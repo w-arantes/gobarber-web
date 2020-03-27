ca722b0b9aa65ab2525610e41dde31b30d2e784d
w-arantes <w.aranteslfc@gmail.com>
2020-03-26 21:03:04 -0300

    chore: update page styling

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,43 +1,40 @@
 import { createGlobalStyle } from 'styled-components';
-
-import 'react-perfect-scrollbar/dist/css/styles.css';
 import 'react-toastify/dist/ReactToastify.css';
+import 'react-perfect-scrollbar/dist/css/styles.css';
 
 export default createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
-
   * {
     margin: 0;
     padding: 0;
-    outline: 0;
+    outline: none;
     box-sizing: border-box;
   }
-
   *:focus {
-    outline: 0;
+    outline: none;
   }
-
-  html, body, #root {
-    height: 100%;
+  body, html {
+    height: auto;
   }
-
   body {
     -webkit-font-smoothing: antialiased;
   }
-
-  body, input, button, {
-    font: 'Roboto', sans-serif;
+  body, input, button {
+    font: 14px 'Roboto', sans-serif;
   }
-
   a {
     text-decoration: none;
   }
-
   ul {
     list-style: none;
   }
-
   button {
     cursor: pointer;
+  }
+  hr {
+    border: 0;
+    height: 1px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 10px 0 20px;
   }
 `;
